63b53e5a367aa5d41833a2f7a07b75b2b9e0c641
AlphaLawless <alpha.vylly12@gmail.com>
2026-03-31 01:02:59 -0300

    build: switch SvelteKit adapter from Cloudflare to Auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { relative, sep } from 'node:path';
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -17,19 +17,7 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter({
-      config: undefined,
-      platformProxy: {
-        configPath: undefined,
-        environment: undefined,
-        persist: undefined,
-      },
-      fallback: 'plaintext',
-      routes: {
-        include: ['/*'],
-        exclude: ['<all>'],
-      },
-    }),
+    adapter: adapter({}),
   },
 };
 
